16d318a1ae6838430b8a6a245f43e8966ac8b385
paleto30 <paleto0817@gmail.com>
2023-10-07 14:55:21 -0500

    feat (tools): :sparkles: se creron funciones para invocar alertas segun el caso
    se elimino el componente de alertas que se estaba pensando implementar , se remplazo por las funciones que invocan alertas

---
+++ src/shared/js/alertabase.js
@@ -0,0 +1,30 @@
+
+import Swal from 'sweetalert2';
+
+
+
+// alerta sencilla 
+export const invokeAlert = (title, text, icon, textButton, buttonColor) => {
+    Swal.fire({
+        title: title,
+        text: text,
+        icon: icon,
+        confirmButtonText: textButton,
+        confirmButtonColor: buttonColor
+    })
+}
+
+//invokeAlert('Excelente!',"results.message",'success','Entendido','#2280E5');
+//invokeAlert('Error!',"results.message",'error','Entendido','#2280E5');
+
+
+export const alertProccessSuccess = (title) => {
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        text: title,
+        showConfirmButton: false,
+        timer: 1500,
+        timerProgressBar: true
+    })
+}
