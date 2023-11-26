d3a43b938b3f22a65a16fc76482bc15daa670f27
paleto30 <paleto0817@gmail.com>
2023-11-26 15:39:24 -0500

    refactor ( move ) :zap: se traslado a componentes compartidos

---
+++ src/shared/components/BarraMenus.vue
@@ -0,0 +1,63 @@
+<!-- html -->
+<template>
+    <div class="menuss">
+        <ul class="navbar-nav">
+            <li class="lista" v-for="(item, i) in props.menus" :key="i">
+                <router-link class="nav-link linnk" :to="{ name: item.nameRoute }" ><span :class="item.icon"></span> {{ item.text }}</router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+
+<!-- Js -->
+<script setup>
+
+
+
+// props del menu
+const props = defineProps({
+    menus: Array
+});
+
+
+
+
+
+
+
+</script>
+
+
+<!-- CSS -->
+<style scoped>
+.menuss {
+    /*   border: 0.1px  solid black; */
+    padding: 5px;
+}
+
+
+.lista{
+    margin: 1px;
+}
+
+.linnk{
+    padding-left: 10px;
+    border-radius: 6px;
+}
+
+
+.router-link-active {
+    background-color: #579b0979;
+    border-radius: 6px;
+}
+
+.linnk:hover{
+    background-color: #579b0979 ;
+}
+
+
+
+
+
+</style>
