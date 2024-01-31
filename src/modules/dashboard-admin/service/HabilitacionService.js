3c7257fc68a8ff70977139a60b5c56c992bd5e19
paleto30 <paleto0817@gmail.com>
2024-01-30 22:09:21 -0500

    feat (new data): :sparkles: se creo una nueva funcion del servicio para  obtener los detalles de la habilitacion

--- src/modules/dashboard-admin/service/HabilitacionService.js
+++ src/modules/dashboard-admin/service/HabilitacionService.js
@@ -1,12 +1,12 @@
 import { useAuthStore } from "../../authentication/stores/authStore.js";
 
 const baseUrl = import.meta.env.VITE_API_GETWAY;
-const url = `${baseUrl}/admins/recovery-list`
+const url = `${baseUrl}/admins`
 
 
 export const getRecoveryList = async (page = 1, amount = 15, objFilter = null) => {
 
-    let endpoint = objFilter ? `${url}?page=${page}&amount=${amount}&${objFilter.filter}=${objFilter.value}` : `${url}?amount=${amount}&page=${page}`;
+    let endpoint = objFilter ? `${url}/recovery-list?page=${page}&amount=${amount}&${objFilter.filter}=${objFilter.value}` : `${url}/recovery-list?amount=${amount}&page=${page}`;
     try {
         const response = await fetch(endpoint, {
             method: 'GET',
@@ -23,3 +23,22 @@ export const getRecoveryList = async (page = 1, amount = 15, objFilter = null) =
 }
 
 
+
+
+export const getRecoveryDetails = async (id_recovery) => {
+    let endpoint = `${url}/recovery-details/${id_recovery}`;
+    try {
+        const response = await fetch(endpoint, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${useAuthStore().accessToken}`
+            }
+        });
+
+        const results = await response.json();
+        return results;
+    } catch (error) {
+        console.log(error);
+    }
+}
