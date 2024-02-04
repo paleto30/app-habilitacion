30fc5525c90e91b9472ef57bc027b167d235d98a
paleto30 <paleto0817@gmail.com>
2024-02-03 20:41:17 -0500

    feat (new comp): :sparkles: se crearon los componentes  de detalles

---
+++ src/modules/dashboard-admin/components/habilitaciones/DetalleHabilitacion.vue
@@ -0,0 +1,67 @@
+<!-- html -->
+<template>
+    <div class="detalleEstudiante">
+        <h3 class="ps-2">Solicitud </h3>
+        <div class="p-2 ">
+            <div class="row">
+                <!-- col 1 -->
+                <div class="col col-md-6">
+                    <div class="mb-2">
+                        <label for="" class="labels">Referencia #:</label>
+                        <input type="text" class="inputs" disabled :value="habilitacion.referencia">
+                    </div>
+                </div>
+                <!-- col2 -->
+                <div class="col col col-md-6 derecha">
+                    <div class="mb-2">
+                        <label for="" class="labels">Fecha de creación:</label>
+                        <input type="datetime-local" class="inputs" disabled :value="habilitacion.fecha" >
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<!-- Js -->
+<script setup>
+
+defineProps(['habilitacion']);
+
+
+</script>
+
+
+<!-- CSS -->
+<style scoped>
+.detalleEstudiante {
+    width: 100%;
+    background-color: rgba(236, 236, 236, 0.986);
+    border: 1px solid rgb(179, 179, 179);
+    border-radius: 10px;
+    padding: 10px;
+    color: black;
+}
+
+
+.derecha {
+    display: flex;
+    flex-direction: column;
+    justify-content: end;
+}
+
+.labels {
+    font-weight: 400;
+}
+
+
+.inputs {
+    border-radius: 6px;
+    width: 100%;
+    border: 1px solid rgb(173, 173, 173);
+    height: 35px;
+    padding-left: 5px;
+    color: rgb(77, 124, 22);
+}
+</style>
