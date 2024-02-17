2fe8294a2a28d8fb986bf4e70c600e54f7f09a9d
paleto30 <paleto0817@gmail.com>
2024-02-16 23:19:43 -0500

    refactor (remove routes) :recycle: se elimino un archivo de rutas que no se uso,
    se arreglo la identacion del archivo

--- src/router/index.js
+++ src/router/index.js
@@ -2,21 +2,20 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '../modules/authentication/stores/authStore.js';
 import authRutes from '../modules/authentication/routes/authRoutes.js';
 import studentsRoutes from '../modules/dashboard/routes/studentsRoutes.js';
-import superRoutes from '../modules/dashboard-super/routes/superRoutes.js';
 import adminRoutes from '../modules/dashboard-admin/routes/adminRoutes.js';
 import ErrorView from '../shared/view/ErrorView.vue';
 
 const routes = [
-  {
-    path: '/',
-    redirect: '/authentication/login',
-  },
-  {
-    path: '/:pathMatch(.*)*',
-    name: 'error',
-    component: ErrorView
-    //redirect: '/authentication/login'
-  },
+    {
+        path: '/',
+        redirect: '/authentication/login',
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'error',
+        component: ErrorView
+        //redirect: '/authentication/login'
+    },
 ]
 
 
@@ -24,30 +23,30 @@ const routes = [
 Array.prototype.push.apply(routes, authRutes);
 Array.prototype.push.apply(routes, studentsRoutes);
 Array.prototype.push.apply(routes, adminRoutes);
-Array.prototype.push.apply(routes, superRoutes);
+
 
 
 
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: routes
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: routes
 })
 
 
-router.beforeEach(async(to, from, next) => {
-  const requiresAuth = to.matched.some(record => record.meta.requiresAuth); // esto retorna true si la ruta requiere auth
-  const isAutenticated = useAuthStore().authUser !== null;  // esto tendra el valor de true si el usuario ya esta auten
-  const role = to.matched.some(record => record.meta.role);
-  const rol = role ? to.matched.find(record => record.meta.role).meta.role : null
+router.beforeEach(async (to, from, next) => {
+    const requiresAuth = to.matched.some(record => record.meta.requiresAuth); // esto retorna true si la ruta requiere auth
+    const isAutenticated = useAuthStore().authUser !== null;  // esto tendra el valor de true si el usuario ya esta auten
+    const role = to.matched.some(record => record.meta.role);
+    const rol = role ? to.matched.find(record => record.meta.role).meta.role : null
 
 
-  if ((requiresAuth && !isAutenticated) || (role && rol !== useAuthStore().authUser.rol)) {
-    next({ name: 'login' });
-  } else {
-    next()
-  }
+    if ((requiresAuth && !isAutenticated) || (role && rol !== useAuthStore().authUser.rol)) {
+        next({ name: 'login' });
+    } else {
+        next()
+    }
 });
 
 
