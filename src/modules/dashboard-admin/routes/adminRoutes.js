4f3743a412bbaf6d2bbef628d43b92a751446037
paleto30 <paleto0817@gmail.com>
2024-02-16 23:24:32 -0500

    feat (new routes): :sparkles: rutas para  el admin y el super admin

--- src/modules/dashboard-admin/routes/adminRoutes.js
+++ src/modules/dashboard-admin/routes/adminRoutes.js
@@ -7,7 +7,7 @@ const adminRoutes = [
         name: 'dashboard-admin',
         component: () => import('../view/DashboardAdminView.vue'),
         redirect: { name: 'view-estudiantes' },
-        children:[
+        children: [
             {
                 path: 'estuiantes',
                 name: 'view-estudiantes',
@@ -20,6 +20,21 @@ const adminRoutes = [
             },
         ],
         meta: { requiresAuth: true, role: 2 },
+        exact: true
+    },
+    {
+        path: '/dashboard-admins',
+        name: 'dashboard-admin-all-access',
+        component: () => import('../view/DashboardAdminView.vue'),
+        redirect: { name: 'view-admins' },
+        children: [
+            {
+                path: 'Administradores',
+                name: 'view-admins',
+                component: () => import('../view/AdministradoresView.vue')
+            },
+        ],
+        meta: { requiresAuth: true, role: 1 },
         exact: true
     },
 ];
